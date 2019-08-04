c3c1d10ed9d2d39a1de1be69b271e09a1504dc00
Reva Yoga Pradana <revapradana@gmail.com>
2019-08-05 00:18:42 +0700

    done unit test on One Way Binding: ViewModel observe View

--- One Way Binding/ViewModel_Observe_View/AutoCalculateMathStatementTests/Calculator/CalculatorViewModelTest.swift
+++ One Way Binding/ViewModel_Observe_View/AutoCalculateMathStatementTests/Calculator/CalculatorViewModelTest.swift
@@ -25,17 +25,19 @@ class CalculatorViewModelTest: QuickSpec {
                 it("should have correct value for result regarding the statements") {
                     
                     let viewController = CalculatorViewController()
+                    
                     viewController.loadView()
+                    expect(viewController.textField.text).toNotEventually(beNil())
+                    expect(viewController.textField.text).toEventually(equal(""))
                     
-                    expect(viewController.textField).toEventuallyNot(beNil())
-                    expect(viewController.resultLabel).toEventuallyNot(beNil())
+                    viewController.viewModel.setupBinding()
                     
-//                    viewController.viewModel.setupBinding()
+                    viewController.textField.insertText("2*9+9")
                     
-//                    viewController.textField.text = "2*9+9"
-                    
-//                    expect(viewController.viewModel.result.value).toEventuallyNot(equal(""), timeout: 10, pollInterval: 0.2)
-//                    expect(viewController.viewModel.result.value).toEventually(equal("27"), timeout: 10, pollInterval: 0.2)
+                    expect(viewController.textField.text).toNotEventually(equal(""))
+                    expect(viewController.textField.text).toEventually(equal("2*9+9"))
+                    expect(viewController.viewModel.result.value).toEventuallyNot(equal(""), timeout: 10, pollInterval: 0.2)
+                    expect(viewController.viewModel.result.value).toEventually(equal("27"), timeout: 10, pollInterval: 0.2)
                 }
             }
         }
