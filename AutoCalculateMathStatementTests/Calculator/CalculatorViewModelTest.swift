2f383fec65b7a30c3af36d1a76d7201f3ba9abca
Reva Yoga Pradana <revapradana@gmail.com>
2019-08-28 15:56:00 +0700

    test the viewmodel with service stubbed (decreasing dependency to outside stakeholder), also add test for reset function

--- AutoCalculateMathStatementTests/Calculator/CalculatorViewModelTest.swift
+++ AutoCalculateMathStatementTests/Calculator/CalculatorViewModelTest.swift
@@ -17,17 +17,48 @@ import Quick
 class CalculatorViewModelTest: QuickSpec {
     override func spec() {
         describe("testing about statements and result") {
-            afterEach {
-                OHHTTPStubs.removeAllStubs()
+            context("about the statement and the result") {
+                it("should have correct value for result regarding the statements") {
+                    stub(condition: { (request:URLRequest) -> Bool in
+                        return request.url?.absoluteString == Endpoints.mathJSURL
+                    }, response: { (request: URLRequest) -> OHHTTPStubsResponse in
+                        // API return String, not JSON
+                        let responseString = "27"
+                        return OHHTTPStubsResponse(data: responseString.data(using: .utf8)!,
+                                                   statusCode: 200,
+                                                   headers: nil)
+                    })
+                    
+                    let viewModel = CalculatorViewModel()
+                    
+                    viewModel.model.statement.value = "2*9+9"
+                    expect(viewModel.model.result.value).toEventuallyNot(beNil())
+                    expect(viewModel.model.result.value).toEventually(equal("27"))
+                }
             }
             
-            it("should have correct value for result regarding the statements") {
-                let viewModel = CalculatorViewModel()
-                
-                viewModel.model.statement.value = "2*9+9"
-                
-                expect(viewModel.model.result.value).toEventuallyNot(beNil(), timeout: 10, pollInterval: 0.2)
-                expect(viewModel.model.result.value).toEventually(equal("27"), timeout: 10, pollInterval: 0.2)
+            context("about the reset function") {
+                it("should working properly") {
+                    stub(condition: { (request:URLRequest) -> Bool in
+                        return request.url?.absoluteString == Endpoints.mathJSURL
+                    }, response: { (request: URLRequest) -> OHHTTPStubsResponse in
+                        // API return String, not JSON
+                        let responseString = "40"
+                        return OHHTTPStubsResponse(data: responseString.data(using: .utf8)!,
+                                                   statusCode: 200,
+                                                   headers: nil)
+                    })
+                    
+                    let viewModel = CalculatorViewModel()
+                    
+                    viewModel.model.statement.value = "2*20+5-5"
+                    expect(viewModel.model.result.value).toEventuallyNot(beNil())
+                    expect(viewModel.model.result.value).toEventually(equal("40"))
+                    
+                    viewModel.reset()
+                    expect(viewModel.model.statement.value).toEventually(beEmpty())
+                    expect(viewModel.model.result.value).toEventually(beEmpty())
+                }
             }
         }
     }
